02c2a4934a4d8b7eb8c2ab60a791da6c649d37bd
Rezwan66 <shaikhrezwan66@gmail.com>
2025-12-22 21:42:06 +0100

    Add query solutions & readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Database Design & SQL Queries
+
+A simple vehicle rental system database, built using PostgreSQL and Beekeeper Studio.
+
+## Entity Relationship Diagram (ERD)
+
+This section outlines the overall ERD of the relational tables in the system. The ERD shows relation between the following tables:
+
+- Users
+- Vehicles
+- Bookings
+
+<iframe width="100%" height="500px" style="box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); border-radius:15px;" allowtransparency="true" allowfullscreen="true" scrolling="no" title="Embedded DrawSQL IFrame" frameborder="0" src="https://drawsql.app/teams/webdev-35/diagrams/vehicle-rental-system/embed"></iframe>
+
+The full public version of the ERD can be found [here](https://drawsql.app/teams/webdev-35/diagrams/vehicle-rental-system).
--- queries.sql
+++ queries.sql
@@ -1,1 +1,31 @@
--- SQL Queries
+-- Query 1: JOIN
+-- Requirement: Retrieve booking information along with Customer name and Vehicle name.
+
+select b.booking_id, u.name as customer_name, v.name as vehicle_name, b.start_date, b.end_date, b.status
+from bookings as b
+inner join users as u using(user_id)
+inner join vehicles as v using(vehicle_id);
+
+-- Query 2: EXISTS
+-- Find all vehicles that have never been booked.
+
+select *
+from vehicles as v
+where not exists (
+  select 1 from bookings as b
+  where b.vehicle_id = v.vehicle_id
+);
+
+-- Query 3: WHERE
+-- Requirement: Retrieve all available vehicles of a specific type (e.g. cars).
+
+select * from vehicles
+where type = 'car';
+
+-- Query 4: GROUP BY and HAVING
+-- Requirement: Find the total number of bookings for each vehicle and display only those vehicles that have more than 2 bookings.
+
+select v.name as vehicle_name, count(*) as total_bookings from bookings as b
+inner join vehicles as v using(vehicle_id)
+group by v.name
+having count(*) > 2;
